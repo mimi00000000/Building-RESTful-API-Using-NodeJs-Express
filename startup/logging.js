ee654479b3021758b0309710b20cf64c97af9940
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-10-21 01:43:39 +0100

    setting up the app

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,10 @@
 const winston = require('winston');
+// require('winston-mongodb');
+require('express-async-errors');
 
-module.exports = function() {
-    // winston logging
-}
+module.exports = winston.createLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+    ]
+});
